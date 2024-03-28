18288ef9a4c48b31cba6001a095b1cf275187c9d
Martin BOUDRAS <mb137582@iut21.u-bourgogne.fr>
2024-03-28 09:29:14 +0100

    Chapitre 3 terminé avec succès

---
+++ src/Controller/MainController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+class MainController
+{
+    #[Route]
+    public function HomePage() : Response
+    {
+        return new Response("<strong>Hello World !</strong>");
+    }
+}
