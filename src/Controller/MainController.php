ee5f95c5eeffa66078cb37ea8daf7eb710fcf86b
Martin BOUDRAS <mb137582@iut21.u-bourgogne.fr>
2024-04-03 14:18:14 +0200

    Fin du C05

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,8 +12,15 @@ class MainController extends AbstractController
     public function HomePage(): Response
     {
         $starshipCount = 457;
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
         return $this->render("main/homepage.html.twig", [
             'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip,
         ]);
     }
 }
