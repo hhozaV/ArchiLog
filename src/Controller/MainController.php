732c54cc416dd0cf2b03078368d1bb6812239cca
Martin BOUDRAS <mb137582@iut21.u-bourgogne.fr>
2024-04-03 14:07:43 +0200

    Ajout de données

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,6 +11,9 @@ class MainController extends AbstractController
     #[Route]
     public function HomePage(): Response
     {
-        return $this->render("main/homepage.html.twig");
+        $starshipCount = 457;
+        return $this->render("main/homepage.html.twig", [
+            'numberOfStarships' => $starshipCount,
+        ]);
     }
 }
