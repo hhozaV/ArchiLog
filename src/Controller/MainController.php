d78177e0403c2085214ae54b20dd3e281e3700f1
Martin BOUDRAS <mb137582@iut21.u-bourgogne.fr>
2024-04-03 14:00:18 +0200

    Premier modèle

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route]
     public function HomePage(): Response
     {
-        return new Response('<strong>Hello World !</strong>');
+        return $this->render("main/homepage.html.twig");
     }
 }
